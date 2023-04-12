dfe2366c19873c3d9a00688f1ca36a4753efcf6c
John Ingve Olsen <johningveolsen@gmail.com>
2023-04-12 18:20:46 +0200

    create schema

---
+++ backend/ent/schema/user.go
@@ -0,0 +1,36 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).Default(uuid.New),
+		field.String("email").Unique(),
+		field.String("secret"),
+		field.String("auth_token").Unique(),
+		field.String("name"),
+		field.Time("created_at").Default(time.Now),
+		field.Time("updated_at").Default(time.Now).UpdateDefault(time.Now),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("posts", Post.Type),
+		edge.To("comments", Comment.Type),
+	}
+}
