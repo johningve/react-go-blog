ad439ad68608e6842850d48e6f1152881d40b0d3
John Ingve Olsen <johningveolsen@gmail.com>
2023-04-13 12:45:18 +0200

    clean up unused files

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,20 @@
+import "@picocss/pico"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
-
-import App from "./App"
-import "./index.css"
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <div>Hello world!</div>,
+	},
+	{
+		path: "/signup",
+		element: "Signup",
+	},
+	{
+		path: "/login",
+		element: "Login",
 	},
 ])
 
