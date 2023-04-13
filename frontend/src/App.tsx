23541111f4b94bd3b6fd890937e73c0edbdf27a7
John Ingve Olsen <johningveolsen@gmail.com>
2023-04-13 12:38:42 +0200

    semicolons begone

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useState } from "react"
 
-import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import "./App.css"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0)
 
 	return (
 		<div className="App">
@@ -26,7 +26,7 @@ function App() {
 			</div>
 			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
 		</div>
-	);
+	)
 }
 
-export default App;
+export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
 
-import App from "./App";
-import "./index.css";
+import App from "./App"
+import "./index.css"
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <div>Hello world!</div>,
+	},
+])
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>,
-);
+)
